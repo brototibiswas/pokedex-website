9f98a592ed6119469f449251187bf6841e48f8dd
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-07-26 00:19:55 -0400

    add colors to bg and card

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import logo from "./assets/Pokemon-Logo.png";
 function App() {
   return (
     <>
+      {/* <div className="gradient-bg"></div> */}
       <div className="main-container">
         <div className="content-container">
           <img className="main-logo" src={logo} />
